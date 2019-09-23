f94006bd74ca4078d9af56769cb7da9c91a10d0c
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-23 17:06:17 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,29 @@
 class Author
+
+     @@all = []
+
+  attr_accessor :name, :songs
+
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def add_song(song)
+    song.artist = self 
+  end
   
-  attr_accessor :name
+  def songs
+    Song.all.select{|song|song.artist == self}
+  end
   
+  def add_song_by_name(song_name)
+      song_name = Song.new(song_name)
+      add_song(song_name)
+  end
   
+  def self.song_count
+      Song.all.length
+  end  
   
-end
