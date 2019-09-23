c80b082b94c87406cea8d48fcf9a7fb91f9b0610
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-23 17:25:04 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,19 +2,19 @@ class Author
 
      @@all = []
 
-  attr_accessor :name, :post
+  attr_accessor :name, :new_post
 
   def initialize(name)
     @name = name
     @@all << self
   end
 
-  def add_post(post)
-    post.author = self 
+  def add_post(new_post)
+    new_post.author = self 
   end
 
   def posts
-    Post.all.select{|post|post.author == self}
+    Post.all.select{|post_|post_.author == self}
   end
 
 
