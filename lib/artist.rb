247a255c6547666a9215fc52243fa25028f8be4e
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-23 17:39:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,12 +9,12 @@ class Artist
     @@all << self
   end
 
-  def add_song(song)
-    song.artist = self 
-  end
-  
   def songs
     Song.all.select{|song|song.artist == self}
+  end
+  
+  def add_song(song)
+    song.artist = self 
   end
   
   def add_song_by_name(song_name)
