97e297b657824a8ff4ff70981fab9a3986e92e44
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-23 17:17:59 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -17,5 +17,16 @@ class Author
     post.author = self 
   end
 
+  def add_post_by_title(post_title)
+      post_title = Post.new(post_title)
+      add_post(post_title)
+  end
+  
+  def self.post_count
+      Post.all.length
+  end 
+
+
+
 end  
   
