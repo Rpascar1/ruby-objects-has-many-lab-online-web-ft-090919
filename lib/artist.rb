bc8fccec933ba82c6ebbb678f47cc1ada319f0db
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-23 16:34:26 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+require'pry'
+class Artist
+
+     @@all = []
+
+  attr_accessor :name, :songs
+
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def add_song(song)
+    song.artist = self 
+  end
+  
+  def songs
+    Song.all.select{|song|song.artist == self}
+  end
+  
+  def add_song_by_name(song_name)
+      song_name = Song.new(song_name)
+      add_song(song_name)
+  end
+  
+  def song_count
+      @@all.length
+  end  
+  
+
+ # describe ".song_count" do
+  #    it "is a class method that returns the total number of songs associated to all #existing artists" do
+     #   expect(Artist.song_count).to eq(3)
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song
+  attr_accessor :name, :artist, :song
+ 
+ @@all = []
+
+ def initialize(name)
+   @name = name
+   @@all << self
+    
+ end
+ 
+ def self.all
+   @@all
+ end
+ 
+  
+end
